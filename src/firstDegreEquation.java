be63d0507cf4dc45a6620ddb9864c6be155ba5aa
ishopjapan <thoitran2107@gmail.com>
2022-07-01 16:12:30 +0700

    baithuchanh4

---
+++ src/firstDegreEquation.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class firstDegreEquation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Double a;
+        Double b;
+        Double c;
+        System.out.println("nhap vao a");
+        a = scanner.nextDouble();
+        System.out.println("nhap vao b");
+        b = scanner.nextDouble();
+        System.out.println("nhap vao c");
+        c = scanner.nextDouble();
+        if (a != 0) {
+            Double x = (c - b)/a;
+            System.out.println(x);
+        }else {
+            if (b == c) {
+                System.out.println("phuong trinh vo so nghiem");
+            }else {
+                System.out.println("phuong trinh vo nghiem");
+            }
+        }
+    }
+}
